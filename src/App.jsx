6d773190009b2e7b89eedb6d59c70ec8bc0ee75a
ihossen016 <ihossen016@gmail.com>
2023-03-03 10:33:32 +0600

    sort function added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
+import { useState } from "react";
 import "./App.css";
 import DataList from "./components/DataList";
 import Slides from "./components/Slides";
 
 function App({ slides, userList }) {
+    const [data, setData] = useState(userList);
+
+    const value = () => {
+        setData(
+            data.sort((p1, p2) =>
+                p1.value < p2.value ? 1 : p1.value > p2.value ? -1 : 0
+            )
+        );
+        console.log(data);
+    };
+
     return (
         <div className="App">
             {/* <Slides slides={slides} /> */}
 
             <div className="btnGrp">
-                <button>sort by value</button>
+                <button onClick={value}>sort by value</button>
                 <button>sort by Date</button>
             </div>
 
-            <DataList userList={userList} />
+            <DataList userList={data} />
         </div>
     );
 }
